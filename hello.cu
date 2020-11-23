f872d7b54acbe2f59af8a117cc83b1a5a56b24ae
wmf1997 <wmf997@126.com>
2020-11-23 22:01:58 +0000

    from haiku os -2

--- hello.cu
+++ hello.cu
@@ -1,6 +1,19 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
+__global__ void HelloFromGPU(void) {
+	printf("hello from GPU\n");
+	return;	
+}
+
+void HelloFromCPU(void) {
+	printf("hello from CPU\n");
+	return;	
+}
+
 int main (int argc, const char** argv, const char** env) {
-	
+	HelloFromCPU();
+	HelloFromGPU<<<2, 5>>>();
+	HelloFromCPU();
+	return 0;
 }
